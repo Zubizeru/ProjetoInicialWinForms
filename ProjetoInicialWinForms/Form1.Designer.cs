fb186be0093fb7667b9880f185b10e271941ea90
Zubizeru <jose.ehmendes@senacsp.edu.br>
2025-03-14 21:13:46 -0300

    feat: algumas validações

--- ProjetoInicialWinForms/Form1.Designer.cs
+++ ProjetoInicialWinForms/Form1.Designer.cs
@@ -42,26 +42,43 @@
             txtNomeDaMae = new TextBox();
             txtEmail = new TextBox();
             lblEmail = new Label();
+            txtLogradouro = new TextBox();
+            lblLogradouro = new Label();
+            lblNumero = new Label();
+            txtNumero = new TextBox();
+            lblCep = new Label();
+            txtCep = new MaskedTextBox();
+            lblComplemento = new Label();
+            lblBairro = new Label();
+            txtBairro = new TextBox();
+            txtComplemento = new TextBox();
+            txtCidade = new TextBox();
+            lblCidade = new Label();
+            lblEstado = new Label();
+            txtEstado = new TextBox();
+            lblPais = new Label();
+            txtPais = new TextBox();
+            btnSalvar = new Button();
             SuspendLayout();
             // 
             // txtNome
             // 
-            txtNome.Location = new Point(12, 38);
+            txtNome.Location = new Point(82, 60);
             txtNome.Name = "txtNome";
-            txtNome.Size = new Size(395, 23);
+            txtNome.Size = new Size(300, 23);
             txtNome.TabIndex = 0;
             // 
             // txtNumeroMatricula
             // 
-            txtNumeroMatricula.Location = new Point(111, 92);
+            txtNumeroMatricula.Location = new Point(517, 60);
             txtNumeroMatricula.Name = "txtNumeroMatricula";
-            txtNumeroMatricula.Size = new Size(159, 23);
+            txtNumeroMatricula.Size = new Size(119, 23);
             txtNumeroMatricula.TabIndex = 2;
             // 
             // lblNome
             // 
             lblNome.AutoSize = true;
-            lblNome.Location = new Point(12, 20);
+            lblNome.Location = new Point(82, 42);
             lblNome.Name = "lblNome";
             lblNome.Size = new Size(40, 15);
             lblNome.TabIndex = 4;
@@ -70,7 +87,7 @@
             // lblCPF
             // 
             lblCPF.AutoSize = true;
-            lblCPF.Location = new Point(12, 74);
+            lblCPF.Location = new Point(401, 42);
             lblCPF.Name = "lblCPF";
             lblCPF.Size = new Size(28, 15);
             lblCPF.TabIndex = 5;
@@ -79,7 +96,7 @@
             // lblTelefone
             // 
             lblTelefone.AutoSize = true;
-            lblTelefone.Location = new Point(292, 74);
+            lblTelefone.Location = new Point(401, 89);
             lblTelefone.Name = "lblTelefone";
             lblTelefone.Size = new Size(51, 15);
             lblTelefone.TabIndex = 7;
@@ -88,7 +105,7 @@
             // lblNumeroMatricula
             // 
             lblNumeroMatricula.AutoSize = true;
-            lblNumeroMatricula.Location = new Point(112, 74);
+            lblNumeroMatricula.Location = new Point(518, 42);
             lblNumeroMatricula.Name = "lblNumeroMatricula";
             lblNumeroMatricula.Size = new Size(120, 15);
             lblNumeroMatricula.TabIndex = 6;
@@ -96,34 +113,34 @@
             // 
             // txtCPF
             // 
-            txtCPF.Location = new Point(12, 92);
+            txtCPF.Location = new Point(401, 60);
             txtCPF.Mask = "000.000.000-00";
             txtCPF.Name = "txtCPF";
-            txtCPF.Size = new Size(76, 23);
+            txtCPF.Size = new Size(94, 23);
             txtCPF.TabIndex = 8;
             // 
             // txtTelefone
             // 
-            txtTelefone.Location = new Point(292, 92);
+            txtTelefone.Location = new Point(401, 107);
             txtTelefone.Mask = "(00)00000-0000";
             txtTelefone.Name = "txtTelefone";
-            txtTelefone.Size = new Size(115, 23);
+            txtTelefone.Size = new Size(94, 23);
             txtTelefone.TabIndex = 9;
             txtTelefone.ValidatingType = typeof(int);
             // 
             // txtDataNascimento
             // 
-            txtDataNascimento.Location = new Point(12, 151);
+            txtDataNascimento.Location = new Point(517, 107);
             txtDataNascimento.Mask = "00/00/0000";
             txtDataNascimento.Name = "txtDataNascimento";
-            txtDataNascimento.Size = new Size(114, 23);
+            txtDataNascimento.Size = new Size(119, 23);
             txtDataNascimento.TabIndex = 10;
             txtDataNascimento.ValidatingType = typeof(DateTime);
             // 
             // lblDataNascimento
             // 
             lblDataNascimento.AutoSize = true;
-            lblDataNascimento.Location = new Point(12, 133);
+            lblDataNascimento.Location = new Point(517, 89);
             lblDataNascimento.Name = "lblDataNascimento";
             lblDataNascimento.Size = new Size(114, 15);
             lblDataNascimento.TabIndex = 11;
@@ -132,7 +149,7 @@
             // lblNomeDaMae
             // 
             lblNomeDaMae.AutoSize = true;
-            lblNomeDaMae.Location = new Point(150, 133);
+            lblNomeDaMae.Location = new Point(83, 89);
             lblNomeDaMae.Name = "lblNomeDaMae";
             lblNomeDaMae.Size = new Size(82, 15);
             lblNomeDaMae.TabIndex = 13;
@@ -140,32 +157,189 @@
             // 
             // txtNomeDaMae
             // 
-            txtNomeDaMae.Location = new Point(150, 151);
+            txtNomeDaMae.Location = new Point(83, 107);
             txtNomeDaMae.Name = "txtNomeDaMae";
-            txtNomeDaMae.Size = new Size(257, 23);
+            txtNomeDaMae.Size = new Size(299, 23);
             txtNomeDaMae.TabIndex = 12;
             // 
             // txtEmail
             // 
-            txtEmail.Location = new Point(12, 212);
+            txtEmail.Location = new Point(83, 162);
             txtEmail.Name = "txtEmail";
-            txtEmail.Size = new Size(395, 23);
+            txtEmail.Size = new Size(276, 23);
             txtEmail.TabIndex = 14;
             // 
             // lblEmail
             // 
             lblEmail.AutoSize = true;
-            lblEmail.Location = new Point(12, 194);
+            lblEmail.Location = new Point(83, 144);
             lblEmail.Name = "lblEmail";
             lblEmail.Size = new Size(36, 15);
             lblEmail.TabIndex = 15;
             lblEmail.Text = "Email";
             // 
+            // txtLogradouro
+            // 
+            txtLogradouro.Location = new Point(378, 162);
+            txtLogradouro.Name = "txtLogradouro";
+            txtLogradouro.Size = new Size(258, 23);
+            txtLogradouro.TabIndex = 16;
+            // 
+            // lblLogradouro
+            // 
+            lblLogradouro.AutoSize = true;
+            lblLogradouro.Location = new Point(378, 144);
+            lblLogradouro.Name = "lblLogradouro";
+            lblLogradouro.Size = new Size(69, 15);
+            lblLogradouro.TabIndex = 17;
+            lblLogradouro.Text = "Logradouro";
+            // 
+            // lblNumero
+            // 
+            lblNumero.AutoSize = true;
+            lblNumero.Location = new Point(82, 197);
+            lblNumero.Name = "lblNumero";
+            lblNumero.Size = new Size(51, 15);
+            lblNumero.TabIndex = 19;
+            lblNumero.Text = "Numero";
+            // 
+            // txtNumero
+            // 
+            txtNumero.Location = new Point(82, 215);
+            txtNumero.Name = "txtNumero";
+            txtNumero.Size = new Size(60, 23);
+            txtNumero.TabIndex = 18;
+            // 
+            // lblCep
+            // 
+            lblCep.AutoSize = true;
+            lblCep.Location = new Point(160, 197);
+            lblCep.Name = "lblCep";
+            lblCep.Size = new Size(28, 15);
+            lblCep.TabIndex = 21;
+            lblCep.Text = "CEP";
+            // 
+            // txtCep
+            // 
+            txtCep.Location = new Point(160, 215);
+            txtCep.Mask = "00000-000";
+            txtCep.Name = "txtCep";
+            txtCep.Size = new Size(77, 23);
+            txtCep.TabIndex = 22;
+            // 
+            // lblComplemento
+            // 
+            lblComplemento.AutoSize = true;
+            lblComplemento.Location = new Point(83, 250);
+            lblComplemento.Name = "lblComplemento";
+            lblComplemento.Size = new Size(84, 15);
+            lblComplemento.TabIndex = 25;
+            lblComplemento.Text = "Complemento";
+            // 
+            // lblBairro
+            // 
+            lblBairro.AutoSize = true;
+            lblBairro.Location = new Point(363, 250);
+            lblBairro.Name = "lblBairro";
+            lblBairro.Size = new Size(38, 15);
+            lblBairro.TabIndex = 24;
+            lblBairro.Text = "Bairro";
+            // 
+            // txtBairro
+            // 
+            txtBairro.Location = new Point(363, 268);
+            txtBairro.Name = "txtBairro";
+            txtBairro.Size = new Size(114, 23);
+            txtBairro.TabIndex = 23;
+            // 
+            // txtComplemento
+            // 
+            txtComplemento.Location = new Point(83, 268);
+            txtComplemento.Name = "txtComplemento";
+            txtComplemento.Size = new Size(257, 23);
+            txtComplemento.TabIndex = 26;
+            // 
+            // txtCidade
+            // 
+            txtCidade.Location = new Point(505, 215);
+            txtCidade.Name = "txtCidade";
+            txtCidade.Size = new Size(131, 23);
+            txtCidade.TabIndex = 30;
+            // 
+            // lblCidade
+            // 
+            lblCidade.AutoSize = true;
+            lblCidade.Location = new Point(505, 197);
+            lblCidade.Name = "lblCidade";
+            lblCidade.Size = new Size(44, 15);
+            lblCidade.TabIndex = 29;
+            lblCidade.Text = "Cidade";
+            // 
+            // lblEstado
+            // 
+            lblEstado.AutoSize = true;
+            lblEstado.Location = new Point(382, 197);
+            lblEstado.Name = "lblEstado";
+            lblEstado.Size = new Size(42, 15);
+            lblEstado.TabIndex = 28;
+            lblEstado.Text = "Estado";
+            // 
+            // txtEstado
+            // 
+            txtEstado.Location = new Point(382, 215);
+            txtEstado.Name = "txtEstado";
+            txtEstado.Size = new Size(95, 23);
+            txtEstado.TabIndex = 27;
+            // 
+            // lblPais
+            // 
+            lblPais.AutoSize = true;
+            lblPais.Location = new Point(258, 197);
+            lblPais.Name = "lblPais";
+            lblPais.Size = new Size(28, 15);
+            lblPais.TabIndex = 32;
+            lblPais.Text = "Pais";
+            // 
+            // txtPais
+            // 
+            txtPais.Location = new Point(258, 215);
+            txtPais.Name = "txtPais";
+            txtPais.Size = new Size(101, 23);
+            txtPais.TabIndex = 31;
+            // 
+            // btnSalvar
+            // 
+            btnSalvar.Location = new Point(505, 267);
+            btnSalvar.Name = "btnSalvar";
+            btnSalvar.Size = new Size(131, 23);
+            btnSalvar.TabIndex = 33;
+            btnSalvar.Text = "Salvar";
+            btnSalvar.UseVisualStyleBackColor = true;
+            btnSalvar.Click += btnSalvar_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.InactiveBorder;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnSalvar);
+            Controls.Add(lblPais);
+            Controls.Add(txtPais);
+            Controls.Add(txtCidade);
+            Controls.Add(lblCidade);
+            Controls.Add(lblEstado);
+            Controls.Add(txtEstado);
+            Controls.Add(txtComplemento);
+            Controls.Add(lblComplemento);
+            Controls.Add(lblBairro);
+            Controls.Add(txtBairro);
+            Controls.Add(txtCep);
+            Controls.Add(lblCep);
+            Controls.Add(lblNumero);
+            Controls.Add(txtNumero);
+            Controls.Add(lblLogradouro);
+            Controls.Add(txtLogradouro);
             Controls.Add(lblEmail);
             Controls.Add(txtEmail);
             Controls.Add(lblNomeDaMae);
@@ -180,6 +354,8 @@
             Controls.Add(lblNome);
             Controls.Add(txtNumeroMatricula);
             Controls.Add(txtNome);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -189,7 +365,7 @@
         #endregion
 
         private TextBox txtNome;
-        private TextBox textBox2;
+        private TextBox txtEstado;
         private TextBox txtNumeroMatricula;
         private Label lblNome;
         private Label lblCPF;
@@ -203,5 +379,21 @@
         private TextBox txtNomeDaMae;
         private TextBox txtEmail;
         private Label lblEmail;
+        private TextBox txtLogradouro;
+        private Label lblLogradouro;
+        private Label lblNumero;
+        private TextBox txtNumero;
+        private Label lblCep;
+        private MaskedTextBox txtCep;
+        private Label lblComplemento;
+        private Label lblBairro;
+        private TextBox txtBairro;
+        private TextBox txtComplemento;
+        private TextBox txtCidade;
+        private Label lblCidade;
+        private Label lblEstado;
+        private Label lblPais;
+        private TextBox txtPais;
+        private Button btnSalvar;
     }
 }
--- ProjetoInicialWinForms/Form1.cs
+++ ProjetoInicialWinForms/Form1.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace ProjetoInicialWinForms
 {
     public partial class Form1 : Form
@@ -7,5 +9,111 @@ namespace ProjetoInicialWinForms
             InitializeComponent();
         }
 
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            if (!ValidaNome(txtNome.Text))
+                MessageBox.Show("Nome Inválido");
+
+            if (!ValidaCpf(txtCPF.Text))
+                MessageBox.Show("CPF Inválido");
+
+            if (!ValidaData(txtDataNascimento.Text))
+                MessageBox.Show("Data de nascimento inválida");
+
+            if (!ValidaTelefone(txtTelefone.Text))
+                MessageBox.Show("Telefone Inválido");
+
+            if (!ValidaEmail(txtEmail.Text))
+                MessageBox.Show("Email Inválido");
+
+            //if (!ValidaMatricula(txtNumeroMatricula.Text))
+            //    MessageBox.Show("Numero de matricula Inválido");
+
+            //if (!ValidaNomeDaMae(txtNomeDaMae.Text))
+            //    MessageBox.Show("Nome da mãe Inválido");
+
+            //if (!ValidaLogradouro(txtLogradouro.Text))
+            //    MessageBox.Show("Logradouro Inválido");
+
+            //if (!ValidaNumero(txtNumero.Text))
+            //    MessageBox.Show("Numero de Logradouro Inválido");
+
+            //if (!ValidaCep(txtCep.Text))
+            //    MessageBox.Show("Cep Inválido");
+
+            //if (!ValidaBairro(txtBairro.Text))
+            //    MessageBox.Show("Bairro Inválido");
+
+            //if (!ValidaComplemento(txtComplemento.Text))
+            //    MessageBox.Show("Complemento Inválido");
+
+            //if (!ValidaCidade(txtCidade.Text))
+            //    MessageBox.Show("Cidade Inválido");
+
+            //if (!ValidaEstado(txtEstado.Text))
+            //    MessageBox.Show("Estado Inválido");
+
+            //if (!ValidaPais(txtPais.Text))
+            //    MessageBox.Show("Pais Inválido");
+
+    }
+
+        private bool ValidaNome(string nome)
+        {
+            if (nome == null || nome.Trim() == "")
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private bool ValidaCpf(string cpf)
+        {
+            cpf = cpf.Remove(3, 1);
+            cpf = cpf.Remove(6, 1);
+            cpf = cpf.Remove(9, 1);
+
+            if (cpf == null || cpf.Trim() == "" || !cpf.All(char.IsDigit) || cpf.Length != 11)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private bool ValidaData(string DataNascimento)
+        {
+            DateTime hoje = DateTime.Now;
+            DateTime nascimento;
+
+            bool sucesso = DateTime.TryParse(DataNascimento, out nascimento);
+
+            if (!sucesso || nascimento > hoje)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private bool ValidaTelefone(string telefone)
+        {
+            telefone = telefone.Remove(0, 1);
+            telefone = telefone.Remove(3, 1);
+            telefone = telefone.Remove(7, 1);
+
+            if (telefone == null || telefone.Trim() == "" || !telefone.All(char.IsDigit) || telefone.Length != 11)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private bool ValidaEmail(string email)
+        {
+            if (email == null || email.Trim() == "" || !Regex.IsMatch(email, "/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/"))
+            {
+                return false;
+            }
+            return true;
+        }
     }
 }
